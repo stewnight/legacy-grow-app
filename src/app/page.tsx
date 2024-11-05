7c7cce48809f73aaaf68c5499cac69a1d4e498c7
Stew Night <stewnightingale@gmail.com>
2024-11-06 12:14:14 +1300

    Removed default post type and updated schema based on farmos

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,13 @@
-import Link from "next/link";
+import Link from 'next/link'
 
-import { LatestPost } from "~/app/_components/post";
-import { auth } from "~/server/auth";
-import { api, HydrateClient } from "~/trpc/server";
+import { auth } from '~/server/auth'
+import { HydrateClient } from '~/trpc/server'
 
 export default async function Home() {
-  const hello = await api.post.hello({ text: "from tRPC" });
-  const session = await auth();
+  const session = await auth()
 
   if (session?.user) {
-    void api.post.getLatest.prefetch();
+    // void api.post.getLatest.prefetch()
   }
 
   return (
@@ -44,26 +42,20 @@ export default async function Home() {
             </Link>
           </div>
           <div className="flex flex-col items-center gap-2">
-            <p className="text-2xl text-white">
-              {hello ? hello.greeting : "Loading tRPC query..."}
-            </p>
-
             <div className="flex flex-col items-center justify-center gap-4">
               <p className="text-center text-2xl text-white">
                 {session && <span>Logged in as {session.user?.name}</span>}
               </p>
               <Link
-                href={session ? "/api/auth/signout" : "/api/auth/signin"}
+                href={session ? '/api/auth/signout' : '/api/auth/signin'}
                 className="rounded-full bg-white/10 px-10 py-3 font-semibold no-underline transition hover:bg-white/20"
               >
-                {session ? "Sign out" : "Sign in"}
+                {session ? 'Sign out' : 'Sign in'}
               </Link>
             </div>
           </div>
-
-          {session?.user && <LatestPost />}
         </div>
       </main>
     </HydrateClient>
-  );
+  )
 }
